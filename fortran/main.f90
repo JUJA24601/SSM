c868fce9e9a2c4b7550867c71dd77568ebc0d7b7
JUJA24601 <fountain.14.shima@gmail.com>
2024-04-19 17:24:42 +0900

    trivial fix

--- fortran/main.f90
+++ fortran/main.f90
@@ -1,10 +1,20 @@
 program main
     implicit none
-    integer :: n = 3
+    integer n
     real x, y, z
     integer i
+
+    ! csv からデータを読み込む
     open (17, file="test.csv", status="old")
     read (17, "()")
+    do 
+        read (17, *, end=100) x, y, z
+        n = n + 1
+    end do
+    100 continue
+    rewind(17)
+    print *, "NumRec =", n
+    read (17, '()')
     do i = 1, n
         read (17, *) x, y, z
         print *, x, y, z
