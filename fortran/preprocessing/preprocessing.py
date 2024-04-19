c84d2e74d1ff22eb5c2c125c68a1d2d5e727bcc2
JUJA24601 <fountain.14.shima@gmail.com>
2024-04-19 15:34:33 +0900

    code for preprocessing

--- fortran/preprocessing/preprocessing.py
+++ fortran/preprocessing/preprocessing.py
@@ -5,7 +5,24 @@ from stl import mesh
 
 def main():
     triangles = mesh.Mesh.from_file("sphere.stl").vectors     # 三角形のデータ
-    print(triangles[1])
+    num = triangles.shape[0]        # 三角形の数
+    
+    header = "apex1_x, apex1_y, apex1_z, apex2_x, apex2_y, apex2_z, apex3_x, apex3_y, apex3_z, a, b, p0_x, p0_y, p0_z, n1_x, n1_y, n1_z, n2_x, n2_y, n2_z, n3_x, n3_y, n3_z"
+    csv = np.empty((num, 23))
+    
+    for i in range(num) :
+        cord = triangles[i]
+        side = np.array([distance(cord[0], cord[1]), distance(cord[1], cord[2]), distance(cord[2], cord[0])])
+        a = side.max()
+        p0 = cord[side.argmax()]
+        b = distance(cord[(side.argmax()+2)%3], p0)
+        n1 = (cord[(side.argmax()+1)%3] - p0)/np.linalg.norm(cord[(side.argmax()+1)%3] - p0)
+        n2 = (cord[(side.argmax()+2)%3] - p0)/np.linalg.norm(cord[(side.argmax()+2)%3] - p0)
+        n3 = np.cross(n1,n2)/np.linalg.norm(np.cross(n1,n2))
+        temp = np.concatenate([cord.ravel(), [a], [b], p0, n1, n2, n3])
+        csv[i] = np.copy(temp)
+        
+    np.savetxt('data.csv', csv, delimiter=',', header=header, comments='')
 
 def incenter(triangle) :
     """
